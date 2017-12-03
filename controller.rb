0eecb7292d0f292af1700aeafb23bf4d87000820
Michael <michkna3@gmail.com>
2017-12-02 19:49:34 -0600

    pseudocode added

--- controller.rb
+++ controller.rb
@@ -4,6 +4,9 @@ class Controller
     deck = Deck.new({cards: card_array})
     until deck.is_gone? do
       CardView.show_question(deck.get_current_card)
+      #promp user
+      #if response == correct -> count += 1
     end
+    # View.correctness count/5
   end
 end
